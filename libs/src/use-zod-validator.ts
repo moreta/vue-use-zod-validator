269fa75517bcfb4eee245b0cce2decc31048bec7
jungjoo.seo <moretajoo@gmail.com>
2023-03-11 18:39:59 +0900

    fix: error type & shorter debounce time

--- libs/src/use-zod-validator.ts
+++ libs/src/use-zod-validator.ts
@@ -3,21 +3,23 @@ import type { UnwrapRef } from 'vue'
 import { computed, ref } from 'vue'
 import { watchDebounced } from '@vueuse/core'
 
+type Errors<T> = Record<keyof T, { message: string, errorCode: string }[]>
+
 interface ValidationResult<T> {
   values: UnwrapRef<T>
-  errors: Record<keyof T, string>;
+  errors: Errors<T>;
   isValid: boolean;
 }
 
 export const useZodValidator = <T>(schema: ZodTypeAny, initialValues: T) => {
   const values = ref<T>(initialValues)
-  const errors = ref<Record<keyof T, string>>({} as Record<keyof T, string>)
+  const errors = ref<Errors<T>>({} as Errors<T>)
   const isValid = computed(() => Object.keys(errors.value).length === 0)
 
   const validate = async (): Promise<ValidationResult<T>> => {
     const result = await schema.safeParseAsync(values.value)
     if (result.success) {
-      errors.value = {} as Record<keyof T, string>
+      errors.value = {} as Errors<T>
       return {
         values: values.value,
         errors: errors.value,
@@ -28,6 +30,7 @@ export const useZodValidator = <T>(schema: ZodTypeAny, initialValues: T) => {
         message: issue.message,
         errorCode: issue.code,
       })).fieldErrors
+
       return {
         values: values.value,
         errors: errors.value,
@@ -38,7 +41,7 @@ export const useZodValidator = <T>(schema: ZodTypeAny, initialValues: T) => {
 
   watchDebounced(values, async () => {
     await validate()
-  }, { deep: true, debounce: 300, maxWait: 500 })
+  }, { deep: true, debounce: 150, maxWait: 300 })
 
   return {
     values,
